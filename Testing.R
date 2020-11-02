97d323e9613b8d0b52d3c99b6f6eb226dfe8a3d8
Nikhil Rai <nikhil.rai0212@gmail.com>
2020-11-02 11:12:12 +0000

    learning how to write functions

--- Testing.R
+++ Testing.R
@@ -1,3 +1,14 @@
 print("this file was created within Rstudio")
 
 print("And now it lives on GitHub")
+
+print("This is the first change in the code")
+
+
+myfunction <- function(x,y) 
+  
+{
+  
+  z = x + y
+  
+}
